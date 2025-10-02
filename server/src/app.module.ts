7cf0f619c69d4b0eb3ccfb91152ed6ec57dadf43
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-02 09:48:22 -0300

    feat: Added Sequelize integration to Nest application

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT!) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'secret',
+      database: process.env.DB_NAME || 'nestjs',
+      autoLoadModels: true, // Automatically load models defined below
+      synchronize: true, // Automatically create tables based on models
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
