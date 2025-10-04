fee38b671fa03919ca1a1eb874ccff4bd913ac60
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-04 17:43:39 -0300

    feat: Added create order form to dashboard page

--- src/pages/dashboard/dashboard.component.ts
+++ src/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CreateOrderFormComponent } from '../../components/create-order-form/create-order-form.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CreateOrderFormComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent { }
+export class DashboardComponent {}
