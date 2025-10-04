b4d11674e20e0b132d640b9813ae08660d3a7f6e
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-04 10:57:01 -0300

    feat: Generated dashboard page component and implemented its respective routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { LoginComponent } from '../pages/login/login.component';
 import { RegisterComponent } from '../pages/register/register.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   // Default route (e.g., http://localhost:4200/)
@@ -12,6 +13,9 @@ export const routes: Routes = [
 
   // Route for the Contact page (e.g., http://localhost:4200/register)
   { path: 'register', component: RegisterComponent, title: 'Register' },
+
+  // Route for the Dashboard page (e.g., http://localhost:4200/dashboard)
+  { path: 'dashboard', component: DashboardComponent, title: 'Dashboard' },
 
   // Optional: Wildcard route for a 404 Not Found page (must be the LAST route)
   //   { path: '**', redirectTo: '' }, // Redirects any unmatched path back to home
