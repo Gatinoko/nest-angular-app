be55cae02bcb4882791dbdafd0ef6672a180a229
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-04 11:11:24 -0300

    feat: Generated create order form and update order form components

---
+++ src/components/create-order-form/create-order-form.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-order-form',
+  imports: [],
+  templateUrl: './create-order-form.component.html',
+  styleUrl: './create-order-form.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CreateOrderFormComponent { }
---
+++ src/components/update-order-form/update-order-form.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-order-form',
+  imports: [],
+  templateUrl: './update-order-form.component.html',
+  styleUrl: './update-order-form.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UpdateOrderFormComponent { }
