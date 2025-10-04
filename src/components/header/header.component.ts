3708955686c18c506d67c65b15d64629b8b9c5c4
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-04 10:50:23 -0300

    feat: Updated header component with login, register, logout buttons and welcome message

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,11 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  template: `<header class="bg-blue-400">header here</header> `,
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private router = inject(Router);
+  public authService = inject(AuthService);
+
+  /**
+   * Calls the AuthService logout method and redirects the user.
+   */
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
