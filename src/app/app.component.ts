c9bc0f6f31126c3523ff8684ff4237fd95de0979
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-04 17:40:40 -0300

    refactor: Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegistrationFormComponent } from '../components/registration-form/registration-form.component';
 import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistrationFormComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
