45d41f91b71acfa29a69bc903ab6df375ed1af75
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-05 11:16:46 -0300

    feat: Generated create order and update order pages

---
+++ src/pages/create-order/create-order.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-order',
+  imports: [],
+  templateUrl: './create-order.component.html',
+  styleUrl: './create-order.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CreateOrderComponent {}
---
+++ src/pages/update-order/update-order.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-order',
+  imports: [],
+  templateUrl: './update-order.component.html',
+  styleUrl: './update-order.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UpdateOrderComponent { }
