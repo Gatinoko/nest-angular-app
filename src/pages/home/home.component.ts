b9b5c72b6def4819e7fb90da55bc8f7031481e41
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-06 05:12:15 -0300

    feat: Updated home page

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,8 +3,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  template: `<p>home works!</p>`,
+  template: `<div
+    class="flex flex-col gap-2 max-w-96 mx-auto text-center mt-10"
+  >
+    <h1 class="text-2xl font-semibold">Olá, e bem vindo ao "Orders App".</h1>
+    <h2>
+      Isso é um webapp criado com Angular 19 e NestJs 10 como parte do desafio
+      técnico da Zoppy.
+    </h2>
+  </div>`,
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent { }
+export class HomeComponent {}
