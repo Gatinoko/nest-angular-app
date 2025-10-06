27c59e5de8ad16cba07e3723864ad38c50e2fee1
Gabriel Tinoco <gabrielslai@gmail.com>
2025-10-06 05:15:33 -0300

    feat: Updated login user dto

--- server/src/dto/login-user.dto.ts
+++ server/src/dto/login-user.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginUserDto {
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @IsNotEmpty()
+  @MinLength(8)
   readonly password: string;
 }
